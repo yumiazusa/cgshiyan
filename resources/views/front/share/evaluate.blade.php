67103766286b84e6616feb27eec68745e80f10ef
yumiazusa <yumiazusa@hotmail.com>
2023-11-08 09:15:31 +0800

    3rd

--- resources/views/front/share/evaluate.blade.php
+++ resources/views/front/share/evaluate.blade.php
@@ -21,7 +21,9 @@
 <div style="display:flex;">
 <div id="main" style="margin:60px auto;width:500px;height:720px;;"></div>
 </div>
-<div id="main2" style="position:absolute;top:43%;left:23%;width:210px;height:300px;transform: rotate(38deg);"></div>
+<div id="main2" style="position:absolute;top:44%;left:23%;width:210px;height:300px;transform: rotate(38deg);"></div>
+<div style="position:absolute;top:100%;">
+<img src="images/fen.png" style="width:100%" alt="评分"></div>
 <script type="text/javascript">
   var myChart = echarts.init(document.getElementById('main'));
 </script>
