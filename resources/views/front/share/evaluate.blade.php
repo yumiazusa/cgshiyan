1ac67bcba11b4a7a79dab3785dd21d1c1a0bd754
yumiazusa <yumiazusa@hotmail.com>
2023-11-08 08:48:56 +0800

    3rd

--- resources/views/front/share/evaluate.blade.php
+++ resources/views/front/share/evaluate.blade.php
@@ -8,7 +8,9 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
     <script type="text/javascript" src="js/echarts.min.js"></script>
-    
+<style>
+    body{overflow:hildden;overflow-y:auto;overflow-x: auto;}
+</style>
 
     <title>能力评价</title>
 </head>
