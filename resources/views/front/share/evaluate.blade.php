e8cd4012036478acc9ccc69611230e2c68940808
yumiazusa <yumiazusa@hotmail.com>
2023-11-08 09:25:17 +0800

    3rd

--- resources/views/front/share/evaluate.blade.php
+++ resources/views/front/share/evaluate.blade.php
@@ -21,7 +21,7 @@
 <div style="display:flex;">
 <div id="main" style="margin:60px auto;width:500px;height:720px;;"></div>
 </div>
-<div id="main2" style="position:absolute;top:50%;left:23%;width:210px;height:300px;transform: rotate(38deg);"></div>
+<div id="main2" style="position:absolute;top:49%;left:23%;width:210px;height:300px;transform: rotate(38deg);"></div>
 <div style="position:absolute;top:100%;left:-3%">
 <img src="images/fen.png" style="width:100%" alt="评分"></div>
 <script type="text/javascript">
