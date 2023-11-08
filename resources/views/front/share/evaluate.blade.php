1bf3f755d47d1295d610a272e0ba7c973c30d5dc
yumiazusa <yumiazusa@hotmail.com>
2023-11-08 08:27:54 +0800

    3rd

--- resources/views/front/share/evaluate.blade.php
+++ resources/views/front/share/evaluate.blade.php
@@ -7,138 +7,151 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
-    <script type="text/javascript" src="js/Chart.js"></script>
+    <script type="text/javascript" src="js/echarts.min.js"></script>
+    
 
-<style>
-
-    body{
-        width: 100%;
-        padding: 0;
-
-        margin: 0;
-
-    }
-
-    #canvas-holder{
-
-        width:100%;
-
-    }
-
-</style>
-   
     <title>能力评价</title>
 </head>
 
 <body>
-<div id="canvas-holder">
-
-			<canvas id="chart-area" width="100%" height="100%"/>
-
-		</div>
-
-
-
-
-
-	<script>
-
-	             window.onload = function() {
-
-                 var ctx = document.getElementById("canvas").getContext("2d");
-
-                 ctx.fillRect(10, 10, 20, 20);
-
-             };
-
-
-
-		var doughnutData = [
-
-				{
-
-					value: 300,
-
-					color:"#F7464A",
-
-					highlight: "#FF5A5E",
-
-					label: "Red"
-
-				},
-
-				{
-
-					value: 50,
-
-					color: "#46BFBD",
-
-					highlight: "#5AD3D1",
-
-					label: "Green"
-
-				},
-
-				{
-
-					value: 100,
-
-					color: "#FDB45C",
-
-					highlight: "#FFC870",
-
-					label: "Yellow"
-
-				},
-
-				{
-
-					value: 40,
-
-					color: "#949FB1",
-
-					highlight: "#A8B3C5",
-
-					label: "Grey"
-
-				},
-
-				{
-
-					value: 120,
-
-					color: "#4D5360",
-
-					highlight: "#616774",
-
-					label: "Dark Grey"
-
-				}
-
-
-
-			];
-
-
-
-			window.onload = function(){
-
-				var ctx = document.getElementById("chart-area").getContext("2d");
-
-				window.myDoughnut = new Chart(ctx).Doughnut(doughnutData, {responsive : true});
-
-			};
+<div id="main3" style="position:absolute;top:0%;left:0%;width:400px;height:255px;"></div>
+<div style="display:flex;">
+<div id="main" style="margin:60px auto;width:500px;height:720px;transform: rotate(193deg);"></div>
+</div>
+<div id="main2" style="position:absolute;top:41%;left:23%;width:205px;height:300px;"></div>
+<script type="text/javascript">
+  var myChart = echarts.init(document.getElementById('main'));
+</script>
 
 
 
 
+<script>
+var chartDom = document.getElementById('main');
+var myChart = echarts.init(chartDom);
+var option;
+
+option = {
+  tooltip: {
+    trigger: 'item'
+  },
+  series: [
+    {
+      name: 'Access From',
+      type: 'pie',
+      radius: ['50%', '70%'],
+      avoidLabelOverlap: false,
+      label: {
+        show: false,
+        position: 'center'
+      },
+      emphasis: {
+        label: {
+          show: true,
+          fontSize: 30,
+          fontWeight: 'bold',
+        }
+      },
+      labelLine: {
+        show: false
+      },
+      data: [
+        { value: 65000, name: '知识运用' },
+        { value: 36000, name: '技能操作' },
+        { value: 30000, name: '思考空间' },
+        { value: 38000, name: '创新思维' },
+        { value: 52000, name: '团队合作' },
+        { value: 25000, name: '个性展现' },
+      ]
+    }
+  ]
+};
+
+option && myChart.setOption(option);
+
+
+var chartDom = document.getElementById('main2');
+var myChart = echarts.init(chartDom);
+var option;
+
+option = {
+
+  radar: {
+    // shape: 'circle',
+    indicator: [
+      { name: '知识运用', max: 65000 },
+      { name: '技能操作', max: 36000 },
+      { name: '思考空间', max: 30000 },
+      { name: '创新思维', max: 38000 },
+      { name: '团队合作', max: 52000 },
+      { name: '个性展现', max: 25000 }
+    ]
+  },
+  series: [
+    {
+      name: 'Budget vs spending',
+      type: 'radar',
+      data: [
+        {
+          value: [42000, 30000, 20000, 35000, 50000, 18000],
+          name: 'Allocated Budget'
+        }
+      ]
+    }
+  ]
+};
+
+option && myChart.setOption(option);
+
+var chartDom = document.getElementById('main3');
+var myChart = echarts.init(chartDom);
+var option;
+
+option = {
+ 
+  tooltip: {
+    trigger: 'axis',
+    axisPointer: {
+      type: 'shadow'
+    }
+  },
+//   legend: {},
+  grid: {
+    left: '3%',
+    right: '4%',
+    bottom: '3%',
+    containLabel: true
+  },
+  xAxis: {
+    type: 'value',
+    boundaryGap: [0, 0.01]
+  },
+  yAxis: {
+    type: 'category',
+    data: ['Brazil', 'Indonesia', 'USA', 'India', 'China', 'World']
+  },
+  series: [
+    {
+      name: '2011',
+      type: 'bar',
+      data: [18203, 23489, 29034, 104970, 131744, 630230]
+    },
+    {
+      name: '2012',
+      type: 'bar',
+      data: [19325, 23438, 31000, 121594, 134141, 681807]
+    }
+  ]
+};
+
+option && myChart.setOption(option);
 
 
 
-	</script>
+</script>
 
 
-  
+
 </body>
 
 </html>
